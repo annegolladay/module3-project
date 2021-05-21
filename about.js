4d631fa966add0d2786d4bead633a0c79051859d
Anne Golladay <annegolladay@gmail.com>
2021-05-21 07:49:15 -0600

    Completed parts 1 and 2

--- about.js
+++ about.js
@@ -5,10 +5,20 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	//console.log('form submit');
+	alert('Your form has been submitted successfully!')
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+//Mouseover on Cat Image
+let catImg = document.querySelector('#cat-image')
+
+function catCompliment() {
+	catImg.textContent = alert('I love you!')
+}
+
+catImg.addEventListener('mouseover', catCompliment)
